38eb0c059ee5ac7a6d24e3feca022e05eeb8866a
Michael Disaro <michael.disaro@gmail.com>
2021-06-25 11:43:59 +0200

    refactored versioning traits

--- src/traits/versioning_traits.rs
+++ src/traits/versioning_traits.rs
@@ -1,22 +1,33 @@
 pub trait Versioning {
     fn is_more_recent(self, than: &str) -> bool;
+    fn split_dot_version(self) -> Vec<u8>;
+    fn strip_dash_variant(self) -> String;
 }
 
 impl Versioning for &str {
     fn is_more_recent(self, than: &str) -> bool {
-        let base_version = self
-            .to_string()
-            .split('.')
-            .map(|v| v.parse::<u8>().unwrap())
-            .collect::<Vec<u8>>();
-        let than_version = than
-            .to_string()
-            .split('.')
-            .map(|v| v.parse::<u8>().unwrap())
-            .collect::<Vec<u8>>();
+        let base_version = self.split_dot_version();
+        let than_version = than.split_dot_version();
         for i in 0..base_version.len() {
             return &base_version[i] > &than_version[i];
         }
         return false;
+    }
+
+    fn split_dot_version(self) -> Vec<u8> {
+        return self
+            .to_string()
+            .split('.')
+            .map(|v| v.strip_dash_variant().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+    }
+
+    fn strip_dash_variant(self) -> String {
+        return self
+            .to_string()
+            .split('-')
+            .next()
+            .unwrap_or("0")
+            .to_string();
     }
 }
